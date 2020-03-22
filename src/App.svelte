900216f712df58f7a3391bdc70aaafd667b49c96
Antoine Abt <antoine@abt.im>
2020-03-22 21:53:56 +0100

    feat: load csv data from google sheet

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,30 @@
 <script>
-  let name = 'Console geOrchestra'
+  import List from './List.svelte'
+  import CSV from 'csv.js'
+
+  let items
+  async function loadData() {
+    let response = await fetch(`https://docs.google.com/spreadsheets/d/e/2PACX-1vQbUP2MhLu5P34MJvOq1OqlLRAmQKMCjfILzEF-Ldcu7eF7UY-7f0Z2okTe-iIVUYUalpYhSK-KeM05/pub?output=csv`)
+    const data = await response.text()
+    items = CSV.decode(data)
+  }
+  loadData()
+
 </script>
 
-<h1>Hello {name}!</h1>
+<h1>Inventaire</h1>
+{#if items===undefined}
+  <p>toto</p>
+{:else}
+  {#await items}
+    <p>Loading...</p>
+  {:then value}
+    <List {items}></List>
+  {:catch error}
+    <p>
+      Erreur de chargement du Google Sheet: «{error.message}»
+    </p>
+  {/await}
+{/if}
+
+
---
+++ src/List.svelte
@@ -0,0 +1,30 @@
+<script>
+  export let items
+</script>
+
+<ul>
+  {#each items as item}
+    <li>
+      <span class=name>{item.Nom}</span>
+      <span class=location>{item.Salle}</span>
+    </li>
+  {/each}
+</ul>
+
+<style>
+  ul {
+    margin: 0;
+    padding: 0;
+  }
+  li {
+    display: flex;
+    border: 1px solid #ddd;
+    border-radius: 0.25em;
+    margin: 0.5em 0;
+    padding: 0.25em;
+  }
+  .name {
+    flex: 1
+  }
+</style>
+
