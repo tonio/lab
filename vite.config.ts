f2426f7ecea04ca7375960a613f62b95406490cb
Antoine Abt <antoine@abt.im>
2022-10-31 22:05:55 +0100

    base dir for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/lab/',
   plugins: [svelte({hot: !process.env.VITEST})],
   test: {
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
